a2e71ba498eae59cb76df71219e9c345dd77ad49
Manjesh V-cp2 <manjeshpv@gmail.com>
2020-04-03 11:16:49 +0530

    Init

---
+++ js/dropdown.js
@@ -0,0 +1,5 @@
+$('li.dropdown').hover(function() {
+    $(this).find('ul.dropdown-menu').stop(true, true).delay(100).fadeIn(100);
+  }, function() {
+    $(this).find('.dropdown-menu').stop(true, true).delay(100).fadeOut(100);
+  });
